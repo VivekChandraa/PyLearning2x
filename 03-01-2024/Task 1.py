7235bfa81f463ea2553f428b336b960a5d8292f1
V1VEKKK <vivekchandra093@gmail.com>
2024-02-03 21:33:00 +0530

    Task Complete

---
+++ 03-01-2024/Task 1.py
@@ -0,0 +1,9 @@
+# 1- Task - Take a user input (name, age, roll_number, phone_number) and print the user details.
+name = input("Enter your Name")
+age = input("Enter Your Age")
+roll_number = input("Enter your roll number")
+phone_number = input("Enter your Phone Number")
+print("Hello My name is", name)
+print("Age =", age)
+print("Roll Number =", roll_number)
+print("Phone Number =", phone_number)
