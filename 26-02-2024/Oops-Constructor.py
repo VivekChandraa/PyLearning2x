f4af4189711c89fa197cc4f76a7f0bd963d6a7d6
Vivek Chandra <158705881+VivekChandraa@users.noreply.github.com>
2024-02-27 10:08:20 +0530

    Update

---
+++ 26-02-2024/Oops-Constructor.py
@@ -0,0 +1,17 @@
+class Person:
+    name = 'vivek'
+    age = 16
+
+    def walk(self):
+        a=10
+        print("Hello my name is", self.name)
+        print("hello my age is", self.age)
+
+
+
+amit= Person()
+amit.walk()
+
+
+
+
